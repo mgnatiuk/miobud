b9b627db15c3f74bca1a51dc7cd5c319b3d55281
Mykola Gnatiuk <gmstudio1916@gmail.com>
2025-10-22 01:05:25 +0200

    fix modal with project

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -125,7 +125,7 @@ const ProjectsSection = () => {
                                 <img
                                     src={project.images[currentImageIndex]}
                                     alt={`${project.name} - ${currentImageIndex + 1}`}
-                                    className="w-full h-[40vh] object-contain"
+                                    className="w-full h-[60vh] object-contain"
                                 />
 
                                 {/* Strzałki nawigacji */}
